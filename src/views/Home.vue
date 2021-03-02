3275fec488778ed203b391958f68c73cd099935e
Sesa26 <sesadlamini@gmail.com>
2021-03-03 00:40:30 +0200

    Sesa123

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,16 @@
 <template>
 	<div class="home">
-		<h1>Home</h1>
+		<h1>Organic Chemistry</h1>
 		<mdb-row class="justify-content-center">
 			<mdb-col col="11">
-				<mdb-carousel :interval="8000" slide :items="items" controlls></mdb-carousel>
+				<mdb-carousel :interval="8000" indicator slide :items="2" controlls>
+					<template #1>
+						<img src="@/assets/2.jpg" class="img-fluid" alt="Responsive image">
+					</template>
+					<template #2>
+						<img src="@/assets/3.jpg" class="img-fluid" alt="Responsive image">
+					</template>
+				</mdb-carousel>
 			</mdb-col>
 		</mdb-row>
 
@@ -11,7 +18,7 @@
 			<section class="my-5">
 				<h2 class="h1-responsive font-weight-bold text-center my-5">Why is it so great?</h2>
 				<p class="lead grey-text w-responsive text-center mx-auto mb-5">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+					Organic chemistry is phenomenal because it is the study of life and all the chemical reactions related to life. Most careers apply an understanding of organic chemistry, such as doctors and chemical engineers.
 				</p>
 				<mdb-row class="justify-content-center">
 					<mdb-col md="3" class="md-0 mb-5">
@@ -22,7 +29,7 @@
 							<mdb-col lg="10" md="9" size="10">
 								<router-link :to="{ name:'alkanes'}" class="h4 font-weight-bold">  Alkanes</router-link>
 								<p class="grey-text">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+									An alkane, is an acyclic saturated hydrocarbon. It consists of hydrogen and carbon atoms arranged in a tree structure in which all the carbon-carbon bonds are single.
 								</p>
 							</mdb-col>
 						</mdb-row>
@@ -35,7 +42,7 @@
 							<mdb-col lg="10" md="9" size="10">
 								<router-link :to="{ name:'alkenes'}" class="h4 font-weight-bold">  Alkenes</router-link>							
 								<p class="grey-text">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+									Alkenes are the simplest unsaturated hydrocarbons. They contain one or more reactive double bonds between the carbon atoms.Their general formula is CnH2n.
 								</p>
 							
 							</mdb-col>
@@ -49,7 +56,7 @@
 							<mdb-col lg="10" md="9" size="10">
 								<router-link :to="{ name:'alkynes'}" class="h4 font-weight-bold">  Alkynes</router-link>							
 								<p class="grey-text">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+									An alkyne is an unsaturated hydrocarbon containing atleast one carbon-carbon triple bond. They have a general formula that C2H2n-2.
 								</p>
 								
 							</mdb-col>
@@ -62,30 +69,26 @@
 </template>
 
 <script>
-import { mdbContainer, mdbRow, mdbCol, mdbIcon,  mdbCarousel } from "mdbvue";
+import { mdbContainer, mdbRow, mdbCol, mdbIcon, mdbCarousel, } from "mdbvue";
 export default {
 	name: "FeaturesPage",
 	components: {
 		mdbContainer,
 		mdbRow,
 		mdbCol,
-		mdbIcon,		
-		mdbCarousel,
+		mdbIcon,
+		mdbCarousel,		
 	},
 	data() {
 		return {
 			items: [
 				{
 					img: true,
-					src: "https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg",
+					src: './assets/2.jpg',
 				},
 				{
 					img: true,
-					src: "https://mdbootstrap.com/img/Photos/Slides/img%20(16).jpg",
-				},
-				{
-					img: true,
-					src: "https://mdbootstrap.com/img/Photos/Slides/img%20(17).jpg",
+					src: './assets/3.jpg',
 				},
 			],
 		};
